b4e900e703a318e0f24429fb43a3a0e0676fda39
ItaloBruno <rt.italo.bruno.silva@gmail.com>
2024-04-08 22:58:06 -0300

    feat: adicionando criação de registro no banco de dados toda vez que se acessa localhost:3000

--- node/index.js
+++ node/index.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
 app.get('/', (req, res) => {
+    const mySql = require('mysql')
+    const conexao = mySql.createConnection(config)
+
+    const sqlDeInsercao = `INSERT INTO people(name) values('Italo')`
+    conexao.query(sqlDeInsercao)
+    conexao.end()
+
     res.send('<h1>Full Cycle Rocks!</h1>')
 })
 
